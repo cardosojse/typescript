a11890e9764c3c882806a8583be247067f2b8c1a
José Cardoso <115185989+cardosojse@users.noreply.github.com>
2024-07-08 20:15:30 -0300

    new file/modified

---
+++ src/A007.ts
@@ -0,0 +1,17 @@
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoNumber = squareOf(2);
+const squareOfTwoString = squareOf('2');
+
+if (squareOfTwoString === null) {
+  console.log('Conta inválida');
+} else {
+  console.log(squareOfTwoNumber);
+}
