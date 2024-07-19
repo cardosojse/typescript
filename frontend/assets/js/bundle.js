be9ae02440e616b1524f5635f1bf6c38f517026c
José Cardoso <115185989+cardosojse@users.noreply.github.com>
2024-07-19 10:30:14 -0300

    modified

--- frontend/assets/js/bundle.js
+++ frontend/assets/js/bundle.js
@@ -4,6 +4,40 @@
   !*** ./src/form.ts ***!
   \*********************/
 
+// import isEmail from 'validator/lib/isEmail';
+const SHOW_ERROR_MESSAGE = 'show-error-message';
+const form = document.querySelector('.form');
+const username = document.querySelector('.username');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const password2 = document.querySelector('.password2');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    hideErrorMessages(this);
+    checkForEmptyFields(username, email, password, password2);
+    checkEmail(email);
+});
+function checkForEmptyFields(...inputs) {
+    inputs.forEach((input) => {
+        if (!input.value)
+            showErrorMessage(input, 'O campo está vazio!');
+    });
+}
+function checkEmail(input) {
+    if (!input.value)
+        showErrorMessage(input, 'O email é inválido!');
+}
+function hideErrorMessages(form) {
+    form
+        .querySelectorAll('.' + SHOW_ERROR_MESSAGE)
+        .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGE));
+}
+function showErrorMessage(input, msg) {
+    const formFields = input.parentElement;
+    const errorMessage = formFields.querySelector('.error-message');
+    errorMessage.innerText = msg;
+    formFields.classList.add(SHOW_ERROR_MESSAGE);
+}
 
 /******/ })()
 ;
--- src/form.ts
+++ src/form.ts
@@ -1,10 +1,41 @@
-// TASKS
-// -> Checar se os campos estão vazios.
-// -> Checar se o e-mail está correto.
-// -> Checar se as duas senhas batem.
+// import isEmail from 'validator/lib/isEmail';
 
-const formFields = document.querySelector('.form-fields');
-const userName = document.querySelector('#username');
-const userEmail = document.querySelector('#email');
-const userPassword = document.querySelector('#password');
-const passwordRepeat = document.querySelector('password2');
+const SHOW_ERROR_MESSAGE = 'show-error-message';
+
+const form = document.querySelector('.form') as HTMLFormElement;
+const username = document.querySelector('.username') as HTMLInputElement;
+const email = document.querySelector('.email') as HTMLInputElement;
+const password = document.querySelector('.password') as HTMLInputElement;
+const password2 = document.querySelector('.password2') as HTMLInputElement;
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  hideErrorMessages(this);
+  checkForEmptyFields(username, email, password, password2);
+  checkEmail(email);
+});
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value) showErrorMessage(input, 'O campo está vazio!');
+  });
+}
+
+function checkEmail(input: HTMLInputElement): void {
+  if (!input.value) showErrorMessage(input, 'O email é inválido!');
+}
+
+function hideErrorMessages(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGE)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGE));
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerText = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGE);
+}
